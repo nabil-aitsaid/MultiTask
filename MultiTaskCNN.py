a798155ea2e5c8813834878be89ed335d741e3a2
Nabil AIT SAID <154698353+nabil-aitsaid@users.noreply.github.com>
2024-01-07 21:29:14 +0100

    Add files via upload

--- MultiTaskCNN.py
+++ MultiTaskCNN.py
@@ -9,31 +9,29 @@ Created on Sun Dec 24 18:29:44 2023
 import torch
 import torch.nn as nn
 class MultiTaskCNN(nn.Module):
-    def __init__(self):
+    def __init__(self,filters):
         super(MultiTaskCNN,self).__init__()
-        self.conv = nn.Conv2d(in_channels=1,out_channels=16,kernel_size=(5,5)) #60
-        self.pool=nn.AvgPool2d(kernel_size=(6,6)) # 10
-
+        self.conv1 = nn.Conv2d(in_channels=1,out_channels=filters,kernel_size=(5,5)) #output form = 60*60 pixels
+        self.pool1=nn.AvgPool2d(kernel_size=(5,5)) # #output form = 12*12 pixels
         self.relu=nn.LeakyReLU()
-
-        self.fc1=nn.Linear(in_features=1600,out_features=256)
+        dims =  filters*12**2
+        self.fc1=nn.Linear(in_features=dims,out_features=dims//2)
         self.dropout= nn.Dropout()
-        self.fc21=nn.Linear(in_features=256,out_features=10)
-        self.fc22=nn.Linear(in_features=256,out_features=10)
+        self.fc21=nn.Linear(in_features=dims//2,out_features=10)
+        self.fc22=nn.Linear(in_features=dims//2,out_features=10)
      
 
     def forward(self,x):
-        x = self.conv(x)
+        x = self.conv1(x)
         x = self.relu(x)
-        x = self.pool(x)
+        x = self.pool1(x)
+        x = self.relu(x)
         x = torch.flatten(x,start_dim=1)
         x = self.fc1(x)
         x = self.relu(x)
-        
-        x1 =self. dropout(x)
-        x1 = self.fc21(x1)
-
-        x2 =self. dropout(x)
-        x2 = self.fc22(x2)
+        x1=self.dropout(x)
+        x1 = self.fc21(x)
+        x2=self.dropout(x)
+        x2 = self.fc22(x)
         #returned value size : (batch_size,2,10)
         return torch.cat((x1.unsqueeze(1) , x2.unsqueeze(1)),dim=1)
--- Train.py
+++ Train.py
@@ -7,6 +7,7 @@ from MultiTaskLoss import MultiTaskLoss
 import torch.nn as nn
 import  torch.optim
 from visualization import vis
+import itertools
 
 class ExtractFirstChannel(object):
     def __call__(self, img):
@@ -18,9 +19,13 @@ transform = transforms.Compose([
     ExtractFirstChannel(),     # the image is in black & white so we extract only first channel
     transforms.Normalize(mean=[0.5], std=[0.5])  # Normalize the image
 ])
+# loading the training dataset
 traindataset = datasets.ImageFolder(root='/home/nabil/masteriales/datasets/multimnist/train', transform=transform)
 trainloader = DataLoader(traindataset,batch_size=batchsize,shuffle=True)
-
+# loading the validation dataset
+valdataset = datasets.ImageFolder(root='/home/nabil/masteriales/datasets/multimnist/val', transform=transform)
+valloader = DataLoader(valdataset,batch_size=batchsize)
+# loading the test dataset
 testdataset = datasets.ImageFolder(root='/home/nabil/masteriales/datasets/multimnist/test', transform=transform)
 testloader = DataLoader(testdataset,batch_size=batchsize)
 
@@ -29,83 +34,91 @@ number_of_batches = len(trainloader)
 # Check if GPU is available
 if torch.cuda.is_available():
     device = torch.device("cuda")
-    print("GPU is available.")
+    print("GPU is available. Using GPU")
 else:
     device = torch.device("cpu")
     print("GPU is not available. Using CPU.")
 
-# initialize the model
-model = MultiTaskCNN()
-model=model.to(device)
-optimizer = torch.optim.Adam(model.parameters(), lr=0.001)
-loss_fn= MultiTaskLoss()
-print(" --- training step ---")
-model.train()
-epochs = 20
-losses_history = [ ]
-for epoch in range(epochs) :
-    run_loss=0
-
-    for inputs,labels in trainloader:
-        
-        # split the labels ex: "90" => [9,0]
-        labels = torch.tensor([ [int(traindataset.classes[e][0]) , int(traindataset.classes[e][1]) ] for e in labels ])
-        inputs = inputs.to(device)
-        labels = labels.to(device)
-
-        # make predictions
-        outputs=model(inputs)
-        #initialize gradients
-        optimizer.zero_grad()
-        #compute loss
-        loss = loss_fn(outputs,labels)
-        #backpropagation
-        loss.backward()
-        #update weights
-        optimizer.step()
-        run_loss+=loss.item()
+def train(model,epochs):
+    model.train()
+    losses_history = [ ]
+    for epoch in range(epochs) :
+        run_loss=0
     
-    run_loss = run_loss / number_of_batches
-    print(f"Epoch: [{epoch + 1}/{epochs}], Loss: {run_loss}")
-    losses_history.append(run_loss)
+        for inputs,labels in trainloader:
+            
+            # split the labels ex: "90" => [9,0]
+            labels = torch.tensor([ [int(traindataset.classes[e][0]) , int(traindataset.classes[e][1]) ] for e in labels ])
+            inputs = inputs.to(device)
+            labels = labels.to(device)
+    
+            # make predictions
+            outputs=model(inputs)
+            #initialize gradients
+            optimizer.zero_grad()
+            #compute loss
+            loss = loss_fn(outputs,labels)
+            #backpropagation
+            loss.backward()
+            #update weights
+            optimizer.step()
+            run_loss+=loss.item()
+        
+        run_loss = run_loss / number_of_batches
+        print(f"Epoch: [{epoch + 1}/{epochs}], Loss: {run_loss}")
+        losses_history.append(run_loss)
     vis(losses_history)
 
+def evaluate(model,dataset,loader):
+    
+    # evaluation step
+    number_of_batches = len(loader)
+    model.eval()
+    correct_predictions=0
+    total=0
+    for inputs, labels in loader:
+        labels = torch.tensor([ [int(dataset.classes[e][0]) , int(dataset.classes[e][1]) ] for e in labels ])
+        inputs = inputs.to(device)
+        labels = labels.to(device)
+        outputs = model(inputs)
+    
+        for i in range(len(outputs)):
+            total+=1
+            if( (outputs[i,0,:].argmax()==labels[i,0]) and (outputs[i,1,:].argmax()==labels[i,1]) ):
+                correct_predictions+=1
+    accuracy = correct_predictions/total
+    return accuracy
 
-# evaluation step
-print(" --- evaluation step ---")
-print(" --- computing training score ---")
-number_of_batches = len(trainloader)
-model.eval()
-correct_predictions=0
-total=0
-for inputs, labels in trainloader:
-    labels = torch.tensor([ [int(traindataset.classes[e][0]) , int(traindataset.classes[e][1]) ] for e in labels ])
-    inputs = inputs.to(device)
-    labels = labels.to(device)
-    outputs = model(inputs)
+params = {
+    "optimizers" : [torch.optim.RMSprop],
+    "epochs": [20,25],
+    "filters":[32,64]
+ }
+combinations = list(itertools.product(*params.values()))
+best_acc = 0
+# initialize the model
+print(" --- training step ---")
+for i,comb in enumerate(combinations): #for each combination
+    print(f"Model {i+1}/{len(combinations)}")
+    model = MultiTaskCNN(comb[2]) # passing the number of filters paramaeter to the model
+    model=model.to(device) # using the model with GPU
+    optimizer = comb[0](model.parameters(), lr=0.001) 
+    loss_fn= MultiTaskLoss()
+    epochs=comb[1]
+    train(model,epochs) # training the model
+    acc = evaluate(model,valdataset,valloader) # evaluating the model on the validation dataset
+    # selecting the best model and combination
+    if(acc> best_acc):
+        best_acc = acc
+        best_model = model
+        best_comb=comb
 
-    for i in range(len(outputs)):
-        total+=1
-        if( (outputs[i,0,:].argmax()==labels[i,0]) and (outputs[i,1,:].argmax()==labels[i,1]) ):
-            correct_predictions+=1
-accuracy = correct_predictions/total
-print(f'Train Accuracy: {accuracy * 100:.2f}%')
+best_model_parameters=dict(zip(params.keys(),best_comb))
+print(f"best model : {best_model_parameters}")
+acc=evaluate(best_model,traindataset, trainloader)
+print(f'Train Accuracy: {acc * 100:.2f}%')
+acc=evaluate(best_model,valdataset, valloader)
+print(f'Validation Accuracy: {acc * 100:.2f}%')
+acc=evaluate(best_model,testdataset, testloader)
+print(f'Test Accuracy: {acc * 100:.2f}%')
 
-# evaluation step
-print(" --- computing test score ---")
-number_of_batches = len(testloader)
-model.eval()
-correct_predictions=0
-total=0
-for inputs, labels in testloader:
-    labels = torch.tensor([ [int(testdataset.classes[e][0]) , int(testdataset.classes[e][1]) ] for e in labels ])
-    inputs = inputs.to(device)
-    labels = labels.to(device)    
-    outputs = model(inputs)
-
-    for i in range(len(outputs)):
-        total+=1
-        if( (outputs[i,0,:].argmax()==labels[i,0]) and (outputs[i,1,:].argmax()==labels[i,1]) ):
-            correct_predictions+=1
-accuracy = correct_predictions/total
-print(f'Test Accuracy: {accuracy * 100:.2f}%')
